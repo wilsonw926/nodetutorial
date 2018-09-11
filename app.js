78785f1c228f8731efc82ce8abd046ec2c49e52c
Wilson Wang <wilsonw926@gmail.com>
2018-09-11 16:52:04 -0700

    basics stuff, starting logger

---
+++ app.js
@@ -0,0 +1,15 @@
+//say hello
+function sayHello(name) {
+	console.log('Hello ' + name);
+	// console.log = print
+}
+
+//make sure you add semicolons
+sayHello('Wilson');
+
+//variables are not added to global object
+var message = '';
+//function added to global scope
+var function = function() {
+
+}
---
+++ logger.js
@@ -0,0 +1,7 @@
+
+var url = "http://www.google.com";
+
+function log(message) {
+	//Send HTTP request
+	console.log(message);
+}
